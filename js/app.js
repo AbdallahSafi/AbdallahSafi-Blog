f5c0c340e9da4fb4ade6cd84f2ead24b90ccf6bd
Abdallah Safi <mr.abdallahsafi@gmail.com>
2020-06-10 00:01:31 +0300

    adding the 7 questions in the js file

--- js/app.js
+++ js/app.js
@@ -1,28 +1,34 @@
 'use strict;';
 
-welcome();
+// welcome();
 
-function welcome() {
-  var name = prompt('Enter Your Name', 'Guest');
-  var nameElement = document.getElementById('name');
-  var welcom = document.getElementById('welcom');
-  if (name !== '') {
-    welcom.style.display = 'block';
-  }
-  nameElement.innerHTML = name;
-  alert('Hello, ' + name + '. I hope you enjoy your time here');
-  guessGame();
-  alert('Welcome a gain ' +name);
+// function welcome() {
+var Gistname = prompt('Enter Your Name', 'Guest');
+var nameElement = document.getElementById('name');
+var welcom = document.getElementById('welcom');
+if (Gistname !== '') {
+  welcom.style.display = 'block';
 }
+nameElement.innerHTML = Gistname;
+alert('Hello, ' + Gistname + '. I hope you enjoy your time here');
+// guessGame();
+// alert('Welcome a gain ' +Gistname);
+// }
 
-function guessGame() {
-  alert('Before I let you enter my website, We will play a guessing game and you should answer with y/n or yes/no only.\n \n let us get started');
-  question1();
-  question2();
-  question3();
-  question4();
-  question5();
-}
+// function guessGame() {
+alert('Before I let you enter my website, We will play a guessing game and you should answer with y/n or yes/no for the first 5 question.\n \n let us get started');
+question1();
+question2();
+question3();
+question4();
+question5();
+// question6();
+// question7();
+// question8();
+// question9();
+// }
+
+// 5 yes or no Questions
 
 function question1(){
   var q1 = 'Am I a male ?';
@@ -98,5 +104,303 @@ function question5(){
     question5();
   }
 }
+
+//   6 numeric Questions
+
+var nca = 0; //counter for the number of correct answer
+// function question6(){
+var q = 'How old I am ?';
+var attemps = 0;
+while(attemps < 4){
+  var a = Number(prompt(q));
+  // console.log(a);
+  switch (true) {
+  case (a === 29):
+    alert('You guessed it right!');
+    nca= nca + 1;
+    // console.log('right');
+    break;
+  case (a > 29):
+    // console.log('high');
+    attemps = attemps + 1;
+    alert('oobs! too high. You have '+(4-attemps)+' tries left');
+    // console.log(attemps);
+    break;
+  case (0 < a && a < 29):
+    // console.log('low');
+    attemps+=1;
+    alert('oobs! too low. You have '+(4-attemps)+' tries left');
+    // console.log(attemps);
+    break;
+  case (a === 0 || isNaN(a)):
+    // console.log('not valid');
+    attemps+=1;
+    alert('You should add a valid number. You have '+(4-attemps)+' tries left');
+    // console.log(attemps);
+    break;
+
+  default:
+    // console.log('default');
+    break;
+  }
+  if (a === 29) {
+    break;
+  }else if(attemps === 4){
+    alert('Sorry, You had four tries. The right answer is 29');
+  }
+}
+
+// }
+
+
+
+// function question7(){
+q = 'I am now married. How many years do you think I was married?';
+attemps = 0;
+while(attemps < 4){
+  a = Number(prompt(q));
+  // console.log(a);
+  switch (true) {
+  case (a === 3):
+    alert('You guessed it right!');
+    nca= nca + 1;
+    // console.log('right');
+    break;
+  case (a > 3):
+    // console.log('high');
+    attemps = attemps + 1;
+    alert('oobs! too high. You have '+(4-attemps)+' tries left');
+    // console.log(attemps);
+    break;
+  case (0 < a && a < 3):
+    // console.log('low');
+    attemps+=1;
+    alert('oobs! too low. You have '+(4-attemps)+' tries left');
+    // console.log(attemps);
+    break;
+  case (a === 0):
+    // console.log('not valid');
+    attemps+=1;
+    alert('You should add a valid number. You have '+(4-attemps)+' tries left');
+    // console.log(attemps);
+    break;
+
+  default:
+    // console.log('default');
+    break;
+  }
+  if (a === 3) {
+    break;
+  }else if(attemps === 4){
+    alert('Sorry, You had four tries. The right answer is 3');
+  }
+}
+// }
+
+
+
+// function question8(){
+q = 'I have been married for 3 years. How many sons do you think I have ?';
+attemps = 0;
+while(attemps < 4){
+  a = Number(prompt(q));
+  // console.log(a);
+  switch (true) {
+  case (a === 1):
+    alert('You guessed it right!');
+    nca= nca + 1;
+    // console.log('right');
+    break;
+  case (a > 1):
+    // console.log('high');
+    attemps = attemps + 1;
+    alert('oobs! too high. You have '+(4-attemps)+' tries left');
+    console.log(attemps);
+    break;
+  case (0 < a && a < 1):
+    // console.log('low');
+    attemps+=1;
+    alert('oobs! too low. You have '+(4-attemps)+' tries left');
+    // console.log(attemps);
+    break;
+  case (a === 0):
+    // console.log('not valid');
+    attemps+=1;
+    alert('You should add a valid number. You have '+(4-attemps)+' tries left');
+    // console.log(attemps);
+    break;
+  default:
+    // console.log('default');
+    break;
+  }
+  if (a === 1) {
+    break;
+  }else if(attemps === 4){
+    alert('Sorry, You had four tries. The right answer is 1');
+
+  }
+}
+
+
+
+// function question9(){
+q = 'What do you think are the total years spent in school in addition to higher education so far?';
+attemps = 0;
+while(attemps < 4){
+  a = Number(prompt(q));
+  // console.log(a);
+  switch (true) {
+  case (a === 19):
+    alert('You guessed it right!');
+    nca= nca + 1;
+    // console.log('right');
+    break;
+  case (a > 19):
+    // console.log('high');
+    attemps = attemps + 1;
+    alert('oobs! too high. You have '+(4-attemps)+' tries left');
+    // console.log(attemps);
+    break;
+  case (0 < a && a < 19):
+    // console.log('low');
+    attemps+=1;
+    alert('oobs! too low. You have '+(4-attemps)+' tries left');
+    // console.log(attemps);
+    break;
+  case (a === 0):
+    // console.log('not valid');
+    attemps+=1;
+    alert('You should add a valid number. You have '+(4-attemps)+' tries left');
+    // console.log(attemps);
+    break;
+  default:
+    // console.log('default');
+    break;
+  }
+  if (a === 19) {
+    break;
+  }else if(attemps === 4){
+    alert('Sorry, You had four tries. The right answer is 19');
+  }
+}
+// }
+
+
+
+// function question10(){
+q = 'What do you think of the number of countries that I visited??';
+attemps = 0;
+while(attemps < 4){
+  a = Number(prompt(q));
+  // console.log(a);
+  switch (true) {
+  case (a === 2):
+    alert('You guessed it right!');
+    nca= nca + 1;
+    // console.log('right');
+    break;
+  case (a > 2):
+    // console.log('high');
+    attemps = attemps + 1;
+    alert('oobs! too high. You have '+(4-attemps)+' tries left');
+    // console.log(attemps);
+    break;
+  case (0 < a && a < 2):
+    // console.log('low');
+    attemps+=1;
+    alert('oobs! too low. You have '+(4-attemps)+' tries left');
+    // console.log(attemps);
+    break;
+  case (a === 0):
+    // console.log('not valid');
+    attemps+=1;
+    alert('You should add a valid number. You have '+(4-attemps)+' tries left');
+    // console.log(attemps);
+    break;
+  default:
+    // console.log('default');
+    break;
+  }
+  if (a === 2) {
+    break;
+  }else if(attemps === 4){
+    alert('Sorry, You had four tries. The right answer is 2');
+  }
+}
+// }
+
+
+// function question11(){
+q = 'Based on what you know about me so far. In your opinion, how many jobs have I worked in previously?';
+attemps = 0;
+while(attemps < 4){
+  a = Number(prompt(q));
+  // console.log(a);
+  switch (true) {
+  case (a === 3):
+    alert('You guessed it right!');
+    nca= nca + 1;
+    // console.log('right');
+    break;
+  case (a > 3):
+    // console.log('high');
+    attemps = attemps + 1;
+    alert('oobs! too high. You have '+(4-attemps)+' tries left');
+    // console.log(attemps);
+    break;
+  case (0 < a && a < 3):
+    // console.log('low');
+    attemps+=1;
+    alert('oobs! too low. You have '+(4-attemps)+' tries left');
+    // console.log(attemps);
+    break;
+  case (a === 0):
+    // console.log('not valid');
+    attemps+=1;
+    alert('You should add a valid number. You have '+(4-attemps)+' tries left');
+    // console.log(attemps);
+    break;
+  default:
+    // console.log('default');
+    break;
+  }
+  if (a === 3) {
+    break;
+  }else if(attemps === 4){
+    alert('Sorry, You had four tries. The right answer is 3');
+  }
+}
+// }
+
+// the 7th question
+var cars =[ 'bmw', 'fiat', 'kia'];
+var correct = false;
+var att = 6;
+while(!correct && att > 0){
+  q = 'What car models do you think I like the most ?';
+  a = prompt(q).toLowerCase();
+  for(var i = 0; i < cars.length; i++){
+    if (a === cars[i]){
+      correct = true;
+      alert('Correct answer!');
+      nca= nca + 1;
+      // console.log('correct');
+    }
+  }
+  if(!correct){
+    att = att - 1;
+    alert('wrong answer, you have '+att+' tries left');
+  }
+}
+var liked ='';
+for( var d = 0; d<cars.length; d++){
+  liked = liked +'-'+cars[d];
+}
+alert('Car models that I like are '+'('+ liked +')' );
+
+alert('Total Correct answers is: '+ nca);
+
+alert('Welcome a gain ' +Gistname);
+
 
 
